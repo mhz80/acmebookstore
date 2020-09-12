04d01d3074508ec51a643b6a5876e38b2bfae4fd
b2l <mhzreg@gmail.com>
2020-09-12 16:51:19 +0430

    AddBookEntity

---
+++ src/Acme.BookStore.Domain.Shared/Books/BookType.cs
@@ -0,0 +1,15 @@
+﻿namespace Acme.BookStore.Books
+{
+    public enum BookType
+    {
+        Undefiend,
+        Advanture,
+        Biography,
+        Dystopia,
+        Fantastic,
+        Horror,
+        Science,
+        ScienceFiction,
+        Poetry
+    }
+}
